48dd0994863e49c43b20f975c52ae99131e0b63c
jay <chlwodnjs34@gmail.com>
2023-05-24 02:53:38 +0900

    feat: 무한 롤링 구현

--- src/component/header/sub/SubHeader.ts
+++ src/component/header/sub/SubHeader.ts
@@ -9,21 +9,24 @@ export class SubHeader extends Base {
     this.render(`
         <div class="header__sub">
           <div class="header__sub__rolling rolling-left">
-            <div class="rolling__wrapper">
-              ${this.setRolling(this.store.left.data)}
+            <div class="rolling__wrapper" data-component="leftWrapper" addTransitionend="leftTransitionend">
+              ${this.setRolling("left")}
             </div>
           </div>
           <div class="header__sub__rolling rolling-right">
-            <div class="rolling__wrapper">
-              ${this.setRolling(this.store.right.data)}
+            <div class="rolling__wrapper"  data-component="rightWrapper" addTransitionend="rightTransitionend">
+              ${this.setRolling("right")}
             </div>
           </div>
         </div>
     `);
+    this.setRollingAnimation();
   }
 
-  setRolling(data: RollingDataType[]) {
-    const rolling = data
+  setRolling(type: keyof StoreType) {
+    const store = this.store[type];
+    const itemList = this.setItemList(store);
+    const rolling = itemList
       .map((item) => {
         return `
         <div class="rolling__wrapper__item">
@@ -36,14 +39,60 @@ export class SubHeader extends Base {
         </div>`;
       })
       .join("");
+
     return rolling;
   }
-  // return rolling;
 
-  async fetchData() {
-    const response = await fetch("http://localhost:8080/rolling");
-    const data = await response.json();
-    return data;
+  setRollingAnimation() {
+    setInterval(() => {
+      this.setTransform("left");
+    }, 5000);
+
+    setTimeout(() => {
+      setInterval(() => {
+        this.setTransform("right");
+      }, 5000);
+    }, 1000);
+  }
+
+  setItemList(store: RollingDataState) {
+    const currentIndex = store.currentIndex;
+    const maxIndex = store.data.length - 1;
+
+    if (currentIndex >= maxIndex) {
+      return [store.data[maxIndex], store.data[0]];
+    }
+    return [store.data[currentIndex], store.data[currentIndex + 1]];
+  }
+
+  setTransform(target: keyof StoreType) {
+    const component = this.components[`${target}Wrapper`];
+    const currentIndex = this.store[target].currentIndex;
+    const dataLength = this.store[target].data.length;
+
+    component.style.transition = "transform 1s";
+    component.style.transform = "translateY(-49px)";
+    this.store[target].currentIndex++;
+    if (currentIndex === dataLength - 1) {
+      this.store[target].currentIndex = 0;
+    }
+  }
+
+  setTransitionend(target: keyof StoreType) {
+    const component = this.components[`${target}Wrapper`];
+    const rollingTemplate = this.setRolling(target);
+
+    component.style.transform = "translateY(0px)";
+    component.style.transition = "none";
+    component.innerHTML = "";
+    component.innerHTML = rollingTemplate;
+  }
+
+  leftTransitionend() {
+    this.setTransitionend("left");
+  }
+
+  rightTransitionend() {
+    this.setTransitionend("right");
   }
 }
-
