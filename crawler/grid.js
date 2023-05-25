72ebabe4fddd60ca902a81a375dfbd6d0d8ce6a6
hjsong <song6240@naver.com>
2023-05-26 02:05:24 +0900

    feat: add inserting id to gridData in crawler

--- crawler/grid.js
+++ crawler/grid.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 
 (async () => {
-  const browser = await puppeteer.launch({headless: "true"});
+  const browser = await puppeteer.launch({ headless: 'true' });
   const page = await browser.newPage();
   await page.goto('https://www.naver.com');
 
@@ -10,18 +10,27 @@ const fs = require('fs');
 
   for (let i = 0; i < 4; i++) {
     const data = await page.evaluate(() => {
-      const newsStandGrid = document.querySelector('.MediaSubscriptionView-module__subscription_group___B8kWW');
+      const newsStandGrid = document.querySelector(
+        '.MediaSubscriptionView-module__subscription_group___B8kWW'
+      );
       const imgs = newsStandGrid.querySelectorAll('img');
 
       return Array.from(imgs).map((img) => {
         return {
           src: img.src,
           alt: img.alt
-        }
-      })
-    })
+        };
+      });
+    });
 
-    brandMarks.push(...data);
+    brandMarks.push(
+      ...data.map((d, index) => {
+        return {
+          id: brandMarks.length + index + 1,
+          ...d
+        };
+      })
+    );
     await page.click('.ContentPagingView-module__btn_next___ZBhby');
   }
 
