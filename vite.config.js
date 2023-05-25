e6353551e240d2de2835e3bda671f2bc0fe6ec1d
Daeram Chung <daeram.chung@gmail.com>
2023-05-25 16:59:31 +0900

    Add path aliases

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
+import path from "path";
+
 export default {
   server: {
     open: true,
+  },
+  resolve: {
+    alias: {
+      "@components": path.resolve(__dirname, "src/components"),
+      "@styles": path.resolve(__dirname, "src/styles"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+    },
   },
 };
