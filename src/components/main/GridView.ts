e1021313a1305d80ca12f4d04f73526044a7d32d
hjsong <song6240@naver.com>
2023-05-26 10:49:58 +0900

    style: create and render arrows in GridView

--- src/components/main/GridView.ts
+++ src/components/main/GridView.ts
@@ -6,9 +6,12 @@ export default class GridView {
   private tbody: HTMLTableSectionElement;
   private rows: HTMLTableRowElement[];
   private cells: HTMLTableCellElement[];
+  private leftArrow: HTMLAnchorElement;
+  private rightArrow: HTMLAnchorElement;
 
   constructor({ gridInfo }: { gridInfo: GridInfo }) {
     this.element = document.createElement('section');
+    this.element.classList.add(style.grid_view);
 
     this.table = document.createElement('table');
     this.table.classList.add(style.table);
@@ -20,23 +23,25 @@ export default class GridView {
 
     const numberOfCells = numberOfRows * numberOfCellsInRow;
     const firstGridIndex = numberOfCells * gridInfo.page;
-    this.cells = gridInfo.imgs.slice(firstGridIndex, firstGridIndex + numberOfCells).map((gridImg) => {
-      const cell = document.createElement('td');
-      cell.classList.add(style.cell);
+    this.cells = gridInfo.imgs
+      .slice(firstGridIndex, firstGridIndex + numberOfCells)
+      .map((gridImg) => {
+        const cell = document.createElement('td');
+        cell.classList.add(style.cell);
 
-      const anchor = document.createElement('a');
-      anchor.href = '#';
-      anchor.classList.add(style.media_thumb);
+        const anchor = document.createElement('a');
+        anchor.href = '#';
+        anchor.classList.add(style.media_thumb);
 
-      const img = document.createElement('img');
-      img.classList.add(style.media_logo);
-      Object.assign(img, { src: gridImg.src, alt: gridImg.alt });
+        const img = document.createElement('img');
+        img.classList.add(style.media_logo);
+        Object.assign(img, { src: gridImg.src, alt: gridImg.alt });
 
-      anchor.append(img);
-      cell.append(anchor);
+        anchor.append(img);
+        cell.append(anchor);
 
-      return cell;
-    });
+        return cell;
+      });
 
     this.rows.forEach((row, index) => {
       const cellsInRow = this.cells.slice(
@@ -46,8 +51,32 @@ export default class GridView {
       row.append(...cellsInRow);
     });
 
+    this.leftArrow = document.createElement('a');
+    this.leftArrow.href = '#';
+    this.leftArrow.classList.add(style.left_arrow);
+    if (gridInfo.page === 0) {
+      this.leftArrow.classList.add('no-display');
+    }
+
+    const leftArrowImg = document.createElement('img');
+    leftArrowImg.src = 'assets/icons/left_arrow.svg'
+
+    this.leftArrow.append(leftArrowImg);
+
+    this.rightArrow = document.createElement('a');
+    this.rightArrow.href = '#';
+    this.rightArrow.classList.add(style.right_arrow);
+    if (gridInfo.page === 3) {
+      this.rightArrow.classList.add('no-display');
+    }
+
+    const rightArrowImg = document.createElement('img');
+    rightArrowImg.src = 'assets/icons/right_arrow.svg'
+
+    this.rightArrow.append(rightArrowImg);
+
     this.tbody.append(...this.rows);
     this.table.append(this.tbody);
-    this.element.append(this.table);
+    this.element.append(this.table, this.leftArrow, this.rightArrow);
   }
 }
