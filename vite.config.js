ea7da5f64c902c8c36e92f63bd071b3e4c21667d
jsh3418 <albert5428@gmail.com>
2023-05-26 10:39:02 +0900

    refactor: assets 디렉토리 path alias 적용

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@style": path.resolve(__dirname, "./src/style"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
       "@components": path.resolve(__dirname, "./src/components"),
     },
   },
