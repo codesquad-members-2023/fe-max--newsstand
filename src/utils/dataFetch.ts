36a1acd4bd1faf76e5281361d7289c57c756d2c4
hjsong <song6240@naver.com>
2023-05-26 16:39:00 +0900

    feat: implement media logo random shuffle
    - Get new gridData when page is refreshed.
    - Apply Fisher-Yates algorithm on getGridData function.

--- src/utils/dataFetch.ts
+++ src/utils/dataFetch.ts
@@ -1,5 +1,9 @@
 import gridData from '../db/grid.json';
 
 export const getGridImgs = () => {
+  for (let i = gridData.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [gridData[i], gridData[j]] = [gridData[j], gridData[i]];
+  }
   return gridData;
 }
