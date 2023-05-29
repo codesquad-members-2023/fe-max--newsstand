214f1d14c902ade1654ffef6f4b2824df3743095
silvertae <massenger7@gmail.com>
2023-05-29 14:47:24 +0900

    feat: 그리드에서 각 언론사 로고 이미지 렌더링 기능 추가
    - GridView 컴포넌트에서 render()를 호출해 state에 있는 정보들을 가지고
    logoUrl을 가져와서 이를 img 요소의 src에 할당한다.
    - state는 data 폴더에 넣어둔 json 데이터를 가져와서 사용하게 했다.
    - 이제 컴포넌트를 파일별로 나누는 작업과 state를 어떻게 전달할지 작업해야한다.

--- src/main.ts
+++ src/main.ts
@@ -1,66 +1,12 @@
 // import { NewsStand } from './components/NewsStand.ts';
+import oneLineNewsData from './data/oneLineData.json';
+import newsStandData from './data/newsStandData.json';
 import './scss/main.scss';
 
 const state = {
-  systemDate: new Date(),
-  oneLines: [
-    {
-      type: 'NEWS-ONELINE',
-      officeName: '연합뉴스',
-      title: "'김남국 방지법' 만장일치 국회통과…의원 코인 재산신고 의무화",
-      url: 'https://news.naver.com/main/list.naver?mode=LPOD&mid=sec&sid1=001&sid2=140&oid=001&isYeonhapFlash=Y&aid=0013964935',
-    },
-    {
-      type: 'NEWS-ONELINE',
-      officeName: '연합뉴스',
-      title: "'전세사기 특별법' 국회통과…최우선변제금만큼 무이자대출",
-      url: 'https://news.naver.com/main/list.naver?mode=LPOD&mid=sec&sid1=001&sid2=140&oid=001&isYeonhapFlash=Y&aid=0013964507',
-    },
-    {
-      type: 'NEWS-ONELINE',
-      officeName: '연합뉴스',
-      title: "'자녀 특혜채용 의혹' 선관위 사무총장·차장 나란히 자진사퇴",
-      url: 'https://news.naver.com/main/list.naver?mode=LPOD&mid=sec&sid1=001&sid2=140&oid=001&isYeonhapFlash=Y&aid=0013964350',
-    },
-    {
-      type: 'NEWS-ONELINE',
-      officeName: '연합뉴스',
-      title: "우크라 '전황' 주시하는 대통령실…무기 지원 현실화하나",
-      url: 'https://news.naver.com/main/list.naver?mode=LPOD&mid=sec&sid1=001&sid2=140&oid=001&isYeonhapFlash=Y&aid=0013963473',
-    },
-    {
-      type: 'NEWS-ONELINE',
-      officeName: '연합뉴스',
-      title: '이창용 "금리인상 옵션 열어놔…못 올릴 거라 생각하면 안 돼"',
-      url: 'https://news.naver.com/main/list.naver?mode=LPOD&mid=sec&sid1=001&sid2=140&oid=001&isYeonhapFlash=Y&aid=0013963828',
-    },
-    {
-      type: 'NEWS-ONELINE',
-      officeName: '연합뉴스',
-      title: '최강 태풍에 괌 韓여행객 수백명 발동동…"방 침수에 지붕 뜯겨"',
-      url: 'https://news.naver.com/main/list.naver?mode=LPOD&mid=sec&sid1=001&sid2=140&oid=001&isYeonhapFlash=Y&aid=0013964762',
-    },
-    {
-      type: 'NEWS-ONELINE',
-      officeName: '연합뉴스',
-      title: "'최저임금' 노사 다시 신경전…인상폭·적용범위 등에서 충돌",
-      url: 'https://news.naver.com/main/list.naver?mode=LPOD&mid=sec&sid1=001&sid2=140&oid=001&isYeonhapFlash=Y&aid=0013964720',
-    },
-    {
-      type: 'NEWS-ONELINE',
-      officeName: '연합뉴스',
-      title: "오늘밤 대법원 앞 '노숙농성' 강행…경찰 강제해산 방침",
-      url: 'https://news.naver.com/main/list.naver?mode=LPOD&mid=sec&sid1=001&sid2=140&oid=001&isYeonhapFlash=Y&aid=0013964930',
-    },
-    {
-      type: 'NEWS-ONELINE',
-      officeName: '연합뉴스',
-      title: '이성만 "이정근에 100만원 준 적 없어…녹취록도 짜깁기"',
-      url: 'https://news.naver.com/main/list.naver?mode=LPOD&mid=sec&sid1=001&sid2=140&oid=001&isYeonhapFlash=Y&aid=0013964520',
-    },
-  ],
+  oneLineNews: oneLineNewsData,
+  newsData: newsStandData,
 };
-
 class NewsStand {
   element: HTMLElement;
   header: Header;
@@ -131,12 +77,12 @@ class NewsRoller {
     const name = document.createElement('div');
     this.name = name;
     name.classList.add('press-name');
-    name.textContent = state.oneLines[0].officeName;
+    name.textContent = state.oneLineNews[0].officeName;
 
     const headLine = document.createElement('div');
     this.title = headLine;
     headLine.classList.add('news-oneline');
-    headLine.textContent = state.oneLines[0].title;
+    headLine.textContent = state.oneLineNews[0].title;
 
     this.element.append(name, headLine);
   }
@@ -199,6 +145,7 @@ class GridView {
   element: HTMLElement;
   leftBtn: HTMLButtonElement;
   rightBtn: HTMLButtonElement;
+  gridItems: HTMLElement[];
 
   constructor() {
     this.element = document.createElement('div');
@@ -211,6 +158,31 @@ class GridView {
     const rightBtn = document.createElement('button');
     this.rightBtn = rightBtn;
     rightBtn.classList.add('btn', 'btn--right');
+
+    this.gridItems = [];
+    const cellNum = 24;
+    for (let i = 0; i < cellNum; i++) {
+      const gridItem = document.createElement('div');
+      gridItem.classList.add('grid-item');
+
+      const logoImg = document.createElement('img');
+      logoImg.classList.add('press-logo');
+
+      gridItem.append(logoImg);
+      this.element.append(gridItem);
+
+      this.gridItems.push(gridItem);
+    }
+
+    this.render();
+  }
+
+  render() {
+    this.gridItems.forEach((item, idx) => {
+      const itemImg = item.children[0] as HTMLImageElement;
+      itemImg.src = state.newsData[idx].logoURL;
+      itemImg.alt = state.newsData[idx].name;
+    });
   }
 }
 
