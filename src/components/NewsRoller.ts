79ed32a3e963fb25ede6697ac39ba256c30e1542
silvertae <massenger7@gmail.com>
2023-05-31 09:59:35 +0900

    feat: 3초마다 넘어가는 뉴스롤러 기능 구현
    - 우선 왼쪽, 오른쪽 시간차 없이 3초마다 넘어가도록 구현
    - NewsRoller 컴포넌트 state에 다음에 보여질 뉴스기사 index 추가
    - @keyframes로 css 애니메이션 적용
    - 타입 에러 해결 필요 (ts2532)

--- src/components/NewsRoller.ts
+++ src/components/NewsRoller.ts
@@ -1,22 +1,65 @@
+import { OneLineNews } from '../types';
+
 export class NewsRoller {
+  private state: {
+    titleIdx: number;
+  };
+  readonly props: OneLineNews[];
   element: HTMLElement;
   name: HTMLDivElement;
-  title: HTMLDivElement;
+  titleBox: HTMLDivElement;
+  currentTitle: HTMLDivElement;
 
-  constructor(props) {
+  constructor(props: OneLineNews[]) {
+    this.state = {
+      titleIdx: 1,
+    };
+    this.props = props;
     this.element = document.createElement('div');
     this.element.classList.add('news-bar');
 
     const name = document.createElement('div');
     this.name = name;
-    name.classList.add('news-bar__press-name');
+    name.classList.add('news-bar__name');
     name.textContent = props[0].name;
 
-    const headLine = document.createElement('div');
-    this.title = headLine;
-    headLine.classList.add('news-bar__title-box');
-    headLine.textContent = props[0].title;
+    const titleBox = document.createElement('div');
+    this.titleBox = titleBox;
+    titleBox.classList.add('news-bar__title-box');
 
-    this.element.append(name, headLine);
+    const title = document.createElement('div');
+    this.currentTitle = title;
+    title.classList.add('title');
+    title.textContent = props[0].title;
+
+    this.titleBox.append(title);
+
+    this.element.append(name, titleBox);
+    this.startRoller();
+  }
+
+  startRoller() {
+    setInterval(() => {
+      const nextTitle = document.createElement('div');
+      nextTitle.classList.add('title');
+      nextTitle.setAttribute('style', 'top: 1rem');
+      nextTitle.textContent = this.props[this.state.titleIdx].title;
+
+      this.state.titleIdx += 1;
+      this.state.titleIdx %= 5;
+
+      this.titleBox.append(nextTitle);
+
+      this.currentTitle.classList.add('roll');
+      nextTitle.classList.add('roll');
+
+      setTimeout(() => {
+        this.titleBox.removeChild(this.currentTitle);
+        this.currentTitle = nextTitle;
+        console.log(this.currentTitle);
+        nextTitle.removeAttribute('style');
+        nextTitle.classList.remove('roll');
+      }, 600);
+    }, 3000);
   }
 }
