7d7b344f731d833e689c6ee0b8d83e720de3ca0b
aaaz425 <aaaz425@naver.com>
2023-06-03 20:02:18 +0900

    feat: add temp file for express
    express 서버를 위한 임시 파일을 추가한다

---
+++ server/server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+import fs from 'fs';
+
+const app = express();
+const port = 8080;
+
+app.use(cors());
+app.get('/rolling', (req, res) => {
+  const data = fs.readFileSync('server/data/rolling.json', 'utf8');
+  res.json(JSON.parse(data));
+});
+
+app.get('/grid', (req, res) => {
+  const data = fs.readFileSync('server/data/grid.json', 'utf8');
+  res.json(JSON.parse(data));
+});
+
+app.get('/list', (req, res) => {
+  const data = fs.readFileSync('server/data/list.json', 'utf8');
+  res.json(JSON.parse(data));
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
