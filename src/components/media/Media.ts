dcabdb75ee4a3f9f74cd60a620553a0f0ebbe09d
bakhacode <bakhamitai@naver.com>
2023-06-05 15:58:54 +0900

    feat: 좌우버튼 리스트,그리드 모드에 따라 개별적으로 동작하게 구현

--- src/components/media/Media.ts
+++ src/components/media/Media.ts
@@ -1,4 +1,5 @@
 import { actions } from "../../actions/actions";
+import { loadNextPress, stopInterval, startInterval, setArt } from "../../app";
 import { getState } from "../../store/store";
 
 export const Media = () => {
@@ -11,37 +12,69 @@ export const Media = () => {
 
   const render = () => {
     const { currentPage, currentLastPage, currentViewMode } = getState();
-    console.log(getState().subsPress);
-    if (currentPage === 1) {
-      prevButton.style.display = "none";
-    } else {
-      prevButton.style.display = "block";
-    }
-    if (currentPage < currentLastPage) {
-      nextButton.style.display = "block";
-    } else {
-      nextButton.style.display = "none";
-    }
 
     renderViewMode(currentViewMode);
+
+    if (currentViewMode === "grid") {
+      prevButton.style.display = currentPage === 1 ? "none" : "block";
+      nextButton.style.display = currentPage < currentLastPage ? "block" : "none";
+      return;
+    }
+    prevButton.style.display = "block";
+    nextButton.style.display = "block";
   };
 
   function renderViewMode(currentViewMode: string) {
-    if (currentViewMode === "grid") {
-      list.style.display = "none";
-      grid.style.display = "grid";
-    } else {
-      grid.style.display = "none";
-      list.style.display = "flex";
-    }
+    const isGridMode = currentViewMode === "grid";
+
+    grid.style.display = isGridMode ? "grid" : "none";
+    gridModeTab.querySelector("path")!.style.fill = isGridMode ? "#4362D0" : "#d2dae0";
+
+    list.style.display = isGridMode ? "none" : "flex";
+    listModeTab.querySelector("path")!.style.fill = isGridMode ? "#d2dae0" : "#4362D0";
   }
 
   const setEvent = () => {
-    prevButton.addEventListener("click", actions.goPrevPage);
-    nextButton.addEventListener("click", actions.goNextPage);
-    gridModeTab.addEventListener("click", actions.switchGridMode);
-    listModeTab.addEventListener("click", actions.switchListMode);
+    prevButton.addEventListener("click", handleClickPrev);
+    // prevButton.addEventListener("click", actions.goPrevPage);
+    nextButton.addEventListener("click", handleClickNext);
+    // nextButton.addEventListener("click", actions.goNextPage);
+    gridModeTab.addEventListener("click", () => {
+      actions.switchGridMode();
+    });
+    listModeTab.addEventListener("click", () => {
+      actions.switchListMode();
+      setArt();
+    });
   };
+
+  function handleClickPrev() {
+    const { currentViewMode } = getState();
+    actions.goPrevPage();
+    currentViewMode === "grid" ? clickPrevGrid() : clickPrevList();
+  }
+  function clickPrevGrid() {
+    console.log("비어있어서 그리드인거 알려줌");
+  }
+  function clickPrevList() {
+    loadNextPress();
+  }
+
+  function handleClickNext() {
+    stopInterval();
+    const { currentViewMode } = getState();
+    actions.goNextPage();
+    currentViewMode === "grid" ? clickNextGrid() : clickNextList();
+    startInterval();
+  }
+  function clickNextGrid() {
+    console.log("비어있어서 그리드인거 알려줌");
+  }
+
+  function clickNextList() {
+    loadNextPress();
+    // renderFieldTab();
+  }
 
   return { setEvent, render };
 };
