c26454ed7b20b9b0530f206d0277f6e01cf93284
aaaz425 <aaaz425@naver.com>
2023-06-07 05:42:10 +0900

    feat: grid 컴포넌트 클래스화 및 플럭스 패턴 적용
    grid 컴포넌트를 클래스화 시키고 flux 패턴을 적용시킨다

--- src/ts/component/App.ts
+++ src/ts/component/App.ts
@@ -1,7 +1,27 @@
+import { $ } from '../common/util';
 import { Header } from './header/Header';
+import { Roller } from './main/rolling/Roller';
+import { Grid } from './main/grid/Grid';
+
+import { store } from '../store';
 
 export class App {
   constructor() {
-    new Header();
+    new Header($('.header__title'), $('.date'));
+
+    this.initRoller();
+    this.initGrid();
+  }
+
+  initRoller() {
+    const roller = new Roller(store.getState());
+    store.subscribe(roller.setLeftRoller);
+    store.subscribe(roller.setRightRoller);
+  }
+
+  initGrid() {
+    const grid = new Grid(store.getState());
+    store.subscribe(grid.setGrid);
+    store.subscribe(grid.hideArrow);
   }
 }
--- src/ts/component/main/grid/Grid.ts
+++ src/ts/component/main/grid/Grid.ts
@@ -1,104 +1,68 @@
-import { $, $$ } from '../../../util/util';
+import { $, $$ } from '../../../common/util';
+import { State } from '../../../common/types';
+import { MAIN } from '../../../common/constant';
+import { ACTION } from '../../../actions';
+import { dispatch } from '../../../dispatch';
 
-type gridInfo = {
-  src: string;
-  alt: string;
-};
-
-async function fetchGridData() {
-  const res = await fetch('http://localhost:8080/grid');
-  const gridData = await res.json();
-  return gridData;
-}
-
-const COUNT_PER_GRID = $$('.grid__items').length;
-
-export async function addEvent() {
-  const gridData = await fetchGridData();
-
-  const leftBtn = $('.grid__left');
-  const rightBtn = $('.grid__right');
-  const arr: Array<gridInfo> = [];
-  const maxPage = await setMaxPage();
-  let counter = 1;
-
-  gridData.forEach((item: gridInfo) => arr.push(item));
-  shuffle(arr);
-  setGrid(arr, counter);
-
-  hideArrow(leftBtn, rightBtn, counter);
-
-  leftBtn.addEventListener('click', () => {
-    if (counter !== 1) {
-      counter--;
-    }
-
-    hideArrow(leftBtn, rightBtn, counter);
-    setGrid(arr, counter);
-  });
-
-  rightBtn.addEventListener('click', () => {
-    if (counter !== maxPage) {
-      counter++;
-    }
-
-    hideArrow(leftBtn, rightBtn, counter);
-    setGrid(arr, counter);
-  });
-}
-
-async function hideArrow(leftBtn: HTMLElement, rightBtn: HTMLElement, counter: number) {
-  const maxPage = await setMaxPage();
-
-  if (counter === 1) {
-    leftBtn.classList.add('hide');
-  } else {
-    leftBtn.classList.remove('hide');
+export class Grid {
+  constructor(private state: State) {
+    this.setGrid(this.state);
+    this.hideArrow(this.state);
+    this.setEvent();
   }
 
-  if (counter === maxPage) {
-    rightBtn.classList.add('hide');
-  } else {
-    rightBtn.classList.remove('hide');
-  }
-}
+  setGrid(state: State) {
+    const grid = $$('.grid__items');
 
-async function setMaxPage() {
-  const gridData = await fetchGridData();
-  return Math.ceil(gridData.length / COUNT_PER_GRID);
-}
+    for (let i = 0; i < grid.length; i++) {
+      const pressCount = i + MAIN.GRID_NUM * (state.curPage - 1);
+      const isOverLength = pressCount >= state.gridList.length;
+      const img = grid[i].querySelector('img');
 
-export async function setGrid(arr: Array<gridInfo>, counter: number) {
-  const grid = $$('.grid__items');
-  const gridData = await fetchGridData();
+      if (!isOverLength) {
+        if (!img) {
+          const newImg = document.createElement('img');
+          grid[i].appendChild(newImg);
+        }
 
-  for (let i = 0; i < grid.length; i++) {
-    const pressCount = i + COUNT_PER_GRID * (counter - 1);
-    const isOverLength = pressCount >= gridData.length;
-    const img = grid[i].querySelector('img');
-
-    if (!isOverLength) {
-      if (!img) {
-        const newImg = document.createElement('img');
-        grid[i].appendChild(newImg);
+        const curImg = grid[i].querySelector('img');
+        curImg?.setAttribute('src', state.gridList[pressCount].src);
+        curImg?.setAttribute('alt', state.gridList[pressCount].alt);
       }
 
-      const curImg = grid[i].querySelector('img');
-      curImg?.setAttribute('src', arr[pressCount].src);
-      curImg?.setAttribute('alt', arr[pressCount].alt);
-    }
-
-    if (isOverLength) {
-      img?.remove();
+      if (isOverLength) {
+        img?.remove();
+      }
     }
   }
-}
 
-function shuffle(array: Array<gridInfo>): Array<gridInfo> {
-  for (let i = 0; i < array.length; i++) {
-    const j = Math.floor(Math.random() * (i + 1));
+  hideArrow(state: State) {
+    const leftBtn = $('.grid__left');
+    const rightBtn = $('.grid__right');
 
-    [array[i], array[j]] = [array[j], array[i]];
+    if (state.curPage === 1) {
+      leftBtn.classList.add('hide');
+    } else {
+      leftBtn.classList.remove('hide');
+    }
+
+    if (state.curPage === state.lastPage) {
+      rightBtn.classList.add('hide');
+    } else {
+      rightBtn.classList.remove('hide');
+    }
   }
-  return array;
+
+  setEvent() {
+    const leftBtn = $('.grid__left');
+    const rightBtn = $('.grid__right');
+
+    leftBtn.addEventListener('click', () => {
+      dispatch({ type: ACTION.GO_TO_PREV });
+    });
+
+    rightBtn.addEventListener('click', () => {
+      dispatch({ type: ACTION.GO_TO_NEXT });
+    });
+  }
 }
