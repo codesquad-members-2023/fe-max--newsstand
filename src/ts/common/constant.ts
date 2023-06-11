1b3d665ccc09bd67b00cb82aa5262a42c42c706a
aaaz425 <aaaz425@naver.com>
2023-06-12 06:01:09 +0900

    feat: type, 상수 모듈, util 을 추가한다
    필요한 타입들과, 상수, util을 추가한다

--- src/ts/common/constant.ts
+++ src/ts/common/constant.ts
@@ -1,8 +1,25 @@
 export const URL = {
   BASE_SERVER: 'http://localhost:8080',
   ROLLING: '/rolling',
-  GRID: '/grid',
   LIST: '/list',
+  GRID: '/grid',
+};
+
+export const VIEW = {
+  ALL: 'VIEW_ALL',
+  SUBS: 'VIEW_SUBS',
+};
+
+export const MODE = {
+  LIST: 'MODE_LIST',
+  GRID: 'MODE_GRID',
+};
+
+export const VIEW_MODE = {
+  ALL_LIST: 'ALL_LIST',
+  ALL_GRID: 'ALL_GRID',
+  SUBS_LIST: 'SUBS_LIST',
+  SUBS_GRID: 'SUBS_GRID',
 };
 
 export const MAIN = {
--- src/ts/common/types.ts
+++ src/ts/common/types.ts
@@ -1,11 +1,66 @@
 export type State = {
+  roller: {
+    leftRollingList: RollingItem[];
+    rightRollingList: RollingItem[];
+    rollerTick: number;
+  };
+
+  viewMode: {
+    view: string;
+    mode: string;
+    viewMode: string;
+  };
+
+  grid: {
+    allGrid: GridItem[];
+    curPage: number;
+    lastPage: number;
+  };
+
+  list: {
+    allList: ListItem[];
+    categoryIndex: number;
+    curCategoryList: ListItem[]; // allList[categoryIndex] = curCategoryList
+    curCategoryIndex: number; // curCategoryList.pressList[curCategoryIndex] = curCategoryItem
+  };
+
+  [key: string]: any;
+};
+
+export type Roller = {
   leftRollingList: RollingItem[];
   rightRollingList: RollingItem[];
   rollerTick: number;
+};
 
-  gridList: GridItem[];
+export type Grid = {
+  allGrid: GridItem[];
   curPage: number;
   lastPage: number;
+};
+
+export type List = {
+  allList: ListItem[];
+  categoryIndex: number;
+  curCategoryList: ListItem[];
+  curCategoryIndex: number;
+};
+
+export type ListItem = {
+  title: string;
+  pressList: PressList[];
+};
+
+export type PressList = {
+  pressLogoSrc: string;
+  pressLogoAlt: string;
+  lastEditted: string;
+  mainArticle: {
+    thumbnailSrc: string;
+    thumbnailAlt: string;
+    mainArticleTitle: string;
+  };
+  subArticles: string[];
 };
 
 export type GridState = {
@@ -16,16 +71,16 @@ export type GridState = {
 
 export type Action = {
   type: string;
-  playload?: any;
-};
-
-export type GridItem = {
-  src: string;
-  alt: string;
+  payload?: any;
 };
 
 export type RollingItem = {
   media: string;
   title: string;
   link: string;
+};
+
+export type GridItem = {
+  src: string;
+  alt: string;
 };
