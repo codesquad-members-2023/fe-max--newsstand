f0c689d47f34231cf1f19c465ecb036e5c671a1a
jsh3418 <albert5428@gmail.com>
2023-06-12 16:25:33 +0900

    chore: jest, moduleNameMapper 설정 추가

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,9 @@
 export default {
   preset: "ts-jest",
   testEnvironment: "node",
+  moduleNameMapper: {
+    "^@utils/(.*)$": "<rootDir>/src/utils/$1",
+    "^@store/(.*)$": "<rootDir>/src/store/$1",
+    "^@components/(.*)$": "<rootDir>/src/components/$1",
+  },
 };
