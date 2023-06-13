64a2ea960fe0c3197d3aa901ec17fe354ffff369
Daeram Chung <daeram.chung@gmail.com>
2023-06-14 01:20:56 +0900

    Remove unnecessary type assertions

--- src/components/NewsStand/NewsStand.ts
+++ src/components/NewsStand/NewsStand.ts
@@ -1,17 +1,13 @@
 import Component from "@components/common/Component.ts";
-import TopHeader from "@components/TopHeader/TopHeader";
-import HeadlinesContainer from "@components/RecentHeadline/HeadlinesContainer";
 
 class NewsStand extends Component {
   constructor() {
     const topElement = document.createElement("div");
     topElement.className = "newsstand-container";
 
-    const topHeader = document.createElement("top-header") as TopHeader;
+    const topHeader = document.createElement("top-header");
 
-    const headlinesContainer = document.createElement(
-      "headlines-container"
-    ) as HeadlinesContainer;
+    const headlinesContainer = document.createElement("headlines-container");
 
     const mainNews = document.createElement("main-news");
 
