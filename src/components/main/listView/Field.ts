1f78a29a4d0cd32b8b254f6e8d85d9e9d6994fd2
hjsong <song6240@naver.com>
2023-06-13 13:12:47 +0900

    feat: progress bar animation on active field tab

--- src/components/main/listView/Field.ts
+++ src/components/main/listView/Field.ts
@@ -5,6 +5,8 @@ export class Field {
   public readonly element = createElement('li', { class: [style.field_tab__item, 'font-body-sm'] });
   private link = createElement('a', { href: '#' });
   private counter = createElement('span');
+  private news: NewsData | null = null;
+  private animationId: number | null = null;
 
   constructor() {
     this.element.append(this.link, this.counter);
@@ -15,5 +17,42 @@ export class Field {
 
     this.element.setAttribute('data-active', field.active.toString());
     this.counter.textContent = field.active ? `${news.order}/${news.categoryCount}` : '';
+    if (field.active && this.news !== news) {
+      this.cancelProgressBarAnimation();
+      this.initProgressBarAnimation();
+      this.news = news;
+    }
+    if (!field.active) {
+      this.cancelProgressBarAnimation();
+      this.element.style.background = '';
+    }
+  }
+
+  initProgressBarAnimation() {
+    let start = performance.now();
+    const step = () => {
+      const now = performance.now();
+      const duration = 3000;
+      const elapsedTime = now - start;
+      const percentage = (elapsedTime / duration) * 100;
+      this.updateProgressBarColor(percentage);
+
+      this.animationId = requestAnimationFrame(step);
+      if (elapsedTime >= duration) {
+        start = performance.now();
+      }
+    };
+    step();
+  }
+
+  updateProgressBarColor(percentage: number) {
+    this.element.style.background = `linear-gradient(90deg, #4362D0 0%, #4362D0 ${percentage}%, #7890E7 ${percentage}%, #7890E7 100%)`;
+  }
+
+  cancelProgressBarAnimation() {
+    if (this.animationId !== null) {
+      cancelAnimationFrame(this.animationId);
+      this.animationId = null;
+    }
   }
 }
